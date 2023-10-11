6caba9ce82971e5a8cd0328e4d43dfa10b277bdf
Bartosz Nowak <bartekn337@gmail.com>
2023-10-11 14:47:42 +0200

    c_bind delete and circuit serialization

--- barretenberg/cpp/src/barretenberg/dsl/acir_proofs/rust_bind.cpp
+++ barretenberg/cpp/src/barretenberg/dsl/acir_proofs/rust_bind.cpp
@@ -1,15 +1,9 @@
-#include "c_bind.hpp"
+#include "rust_bind.hpp"
 #include "../acir_format/acir_to_constraint_buf.hpp"
 #include "acir_composer.hpp"
-#include "barretenberg/common/mem.hpp"
-#include "barretenberg/common/net.hpp"
-#include "barretenberg/common/serialize.hpp"
-#include "barretenberg/common/slab_allocator.hpp"
 #include "barretenberg/dsl/acir_format/acir_format.hpp"
 #include "barretenberg/plonk/proof_system/verification_key/verification_key.hpp"
 #include "barretenberg/srs/global_crs.hpp"
-#include <cstdint>
-#include <memory>
 
 extern "C" {
 

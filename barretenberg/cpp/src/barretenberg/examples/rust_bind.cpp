f0ac5520241ed9b6050f3be9c536327a7d408107
Bartosz Nowak <bartekn337@gmail.com>
2023-10-11 08:44:14 +0200

    rust bindings

---
+++ barretenberg/cpp/src/barretenberg/examples/rust_bind.cpp
@@ -0,0 +1,23 @@
+#include "c_bind.hpp"
+#include "barretenberg/srs/global_crs.hpp"
+#include "simple/simple.hpp"
+
+using namespace proof_system::plonk::stdlib::types;
+
+extern "C" 
+{
+const char* rust_examples_simple_create_and_verify_proof(bool* valid)
+{
+    try {
+    auto ptrs = examples::simple::create_builder_and_composer();
+    auto proof = examples::simple::create_proof(ptrs);
+    *valid = examples::simple::verify_proof(ptrs, proof);
+    examples::simple::delete_builder_and_composer(ptrs);
+    return nullptr;
+        }
+        catch (const std::exception &e)
+        {
+            return e.what(); // return the exception message
+        }
+}
+}
---
+++ barretenberg/cpp/src/barretenberg/srs/rust_bind.hpp
@@ -0,0 +1,4 @@
+#include <barretenberg/common/wasm_export.hpp>
+#include <cstdint>
+
+const char* rust_srs_init_srs(uint8_t const* points_buf, uint32_t const* num_points, uint8_t const* g2_point_buf);
